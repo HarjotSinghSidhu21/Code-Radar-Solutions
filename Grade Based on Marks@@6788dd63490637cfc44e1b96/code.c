10ad9188b385edf9646f8d3b8e0da15710123341
HarjotSinghSidhu21 <harjot1310.becse24@chitkara.edu.in>
2025-01-24 12:25:40 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    char a;
+    scanf("%c",&a);
+    if (a>=90){
+        printf("A");
+    }
+    else if (a>=80 && a<90){
+        printf("B");
+    }
+    else if (a>=70 && a<80){
+        printf("C");
+    }
+    else if (a>=60 && a<70){
+        printf("D");
+    }
+    else{
+        printf("E");
+    }
+    return 0;
+}
